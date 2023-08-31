d11d654a2616162afdd4c533d128e3560884026a
zesk six <zeskingdom6@gmail.com>
2023-08-31 16:52:47 +0800

    navbar added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import Reac from '$lib/images/reac.svg'
+    import Nex from '$lib/images/next.svg'
+    import Github from '$lib/images/iconmonstr-github-1.svg'
+    import '../app.css' 
+
+</script>
+<style>
+    .gradient{
+        background-image: linear-gradient(to right, #3db7c2, #333333 );
+        color: #0000;
+        background-clip: text;
+
+    }
+</style>
+<div class="font-sans">
+    <header class="fixed min-w-screen w-full    flex items-center text-2xl px-5 bg-slate-900" >
+        <div class="flex items-center  basis-1/2">
+            <a href="/" class="flex">
+                <img src={Reac} alt="logo1" width="75px">
+                <img src={Nex} alt="logo2" width="75px">
+            </a>    
+            <span class="gradient font-extrabold text-4xl">nexTutorial</span>
+        </div>
+        <nav class="basis-1/2 flex justify-end items-center">
+            <a href="/react" class="p-0 m-2 text-gray-300 font-bold text-xl hover:text-white      ">
+                React
+            </a>
+            <a href="/next" class="p-0 m-2   text-gray-300 font-bold text-xl hover:text-white    ">
+                Next
+            </a>
+            <hr class="h-[6px] rounded-full w-[6px] flex bg-gray-300 border-none items-center mx-[6px] ">
+            <a href="/games" class="p-0 m-2      text-gray-300 font-bold text-xl hover:text-white ">
+                Games
+            </a>
+            <a href="/blog" class="p-0 m-2      text-gray-300 font-bold text-xl hover:text-white ">
+                controversialOpinions
+            </a>
+            <a href="https://github.com/zesk6/nexTutorial" class="p-0 m-2      ">
+                <img src={Github} alt="github" class="w-[30px]">
+            </a>
+        </nav>
+    </header>
+    <main>
+        <slot />
+    </main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style >
+    .gradient{
+        background-image: linear-gradient(to top, #ee9f27, #ec9006 );
+        color: #0000;
+        background-clip: text; 
+        font-weight: 700;
+    }
+    .gradient3{
+        background-image: linear-gradient(to top, #27ee45, #16901e );
+        color: #0000;
+        background-clip: text; 
+    }
+    .gradient2{
+        background-image: linear-gradient(to top, #e62dc1, #68165c );
+        color: #0000;
+        background-clip: text; 
+    }
+    
+    @keyframes in{
+        from {opacity: 0;}
+        to{opacity: 1;}
+    }
+    .lol{
+        animation-name: in;
+        animation-duration: 1000mx;
+    }
+</style>
+<div class="min-h-screen h-full bg-slate-900 pt-20" >
+    <section class="flex justify-start "> 
+        <article class=" flex flex-col mt-32 ml-20 items-center">
+            <h1 class="text-6xl text-white font-bold m-2 lol">Learn Web-dev</h1>
+            <span class="text-6xl font-bold text-gray-300 m-2 lol">Faster and
+                <span class="gradient group">
+                    Funner.
+                    <span class="absolute scale-0 group-hover:scale-100 bg-slate-900 rounded-lg border border-slate-600 text-white
+                    font-semibold p-2 m-2 text-lg left-1/3 top-1/2 transition duration-400 origin-bottom ">
+                        funner is a word
+                    </span>        
+                </span>
+            </span> 
+            <p class="text-2xl font-semibold text-gray-400 flex flex-col items-center m-2">
+                <span>W3schools without the <span class="gradient2">boredom.</span></span>
+                <span>Fireship.io without the <span class="gradient3">29$/month</span></span>
+            </p>
+            <a href="/react" class="text-white bg-[#2a6786] p-2 text-3xl font-semibold rounded hover:bg-[#295063]
+             transition duration-300 m-4 ">
+                start here
+            </a>
+        </article>
+    </section>  
+
+    
+    
+
+</div>
