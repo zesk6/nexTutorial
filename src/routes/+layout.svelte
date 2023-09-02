d8fbb1444f4cede59fffbf71d02797bab05d0e85
zesk six <zeskingdom6@gmail.com>
2023-09-02 18:46:34 +0800

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
     import Nex from '$lib/images/next.svg'
     import Github from '$lib/images/iconmonstr-github-1.svg'
     import '../app.css' 
+    import YoutubeLogo from '$lib/images/youtubeLogo.svg'
+    import RedditLogo from '$lib/images/redditlogo.svg'
+
+
 
 </script>
 <style>
@@ -12,9 +16,16 @@
         background-clip: text;
 
     }
+    .linear{
+        background-image: linear-gradient(to right, #3db7c2, #333333 );
+
+    }
+
+
+
 </style>
-<div class="font-sans">
-    <header class="fixed min-w-screen w-full    flex items-center text-2xl px-5 bg-slate-900" >
+<div class="font-sans "> 
+    <header class="min-w-screen w-full    flex items-center text-2xl px-5 bg-slate-900" >
         <div class="flex items-center  basis-1/2">
             <a href="/" class="flex">
                 <img src={Reac} alt="logo1" width="75px">
@@ -44,4 +55,27 @@
     <main>
         <slot />
     </main>
+    <footer class="flex flex-col items-center ">
+        <div class="rounded-full w-60  h-1 linear m-5  "></div>
+        <a class="flex items-center m-3" href="/">
+            <div class="flex">
+                <img src={Reac} alt="logo1" width="75px">
+                <img src={Nex} alt="logo2" width="75px">
+            </div>    
+            <span class="gradient font-extrabold text-4xl">nexTutorial</span>
+        </a>
+        <div class="flex items-center justify-between">
+            <!-- change the links later -->
+            <a href="https://youtube.com" class="p-2">
+                <img src={YoutubeLogo} alt="youtube logo" class="w-10 h-10" >
+            </a>
+            <a href="https://github.com" class="p-2">
+                <img src={Github} alt="github logo"     class="w-7 h-7">
+            </a>
+            <a href="https://reddit.com" class="p-2">
+                <img src={RedditLogo} alt="reddit" class="w-10 h-10">
+            </a>
+        </div>
+        <small class="text-sm text-gray-600   ">© Rectanguleur LLM</small>
+    </footer>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,32 +22,54 @@
     }
     .lol{
         animation-name: in;
-        animation-duration: 1000mx;
+        animation-duration: 1000ms;
     }
 </style>
-<div class="min-h-screen h-full bg-slate-900 pt-20" >
-    <section class="flex justify-start "> 
-        <article class=" flex flex-col mt-32 ml-20 items-center">
-            <h1 class="text-6xl text-white font-bold m-2 lol">Learn Web-dev</h1>
-            <span class="text-6xl font-bold text-gray-300 m-2 lol">Faster and
-                <span class="gradient group">
-                    Funner.
-                    <span class="absolute scale-0 group-hover:scale-100 bg-slate-900 rounded-lg border border-slate-600 text-white
-                    font-semibold p-2 m-2 text-lg left-1/3 top-1/2 transition duration-400 origin-bottom ">
-                        funner is a word
-                    </span>        
-                </span>
-            </span> 
-            <p class="text-2xl font-semibold text-gray-400 flex flex-col items-center m-2">
-                <span>W3schools without the <span class="gradient2">boredom.</span></span>
-                <span>Fireship.io without the <span class="gradient3">29$/month</span></span>
-            </p>
-            <a href="/react" class="text-white bg-[#2a6786] p-2 text-3xl font-semibold rounded hover:bg-[#295063]
-             transition duration-300 m-4 ">
-                start here
-            </a>
-        </article>
-    </section>  
+<div class="min-h-screen h-full  pt-15 " >
+        <section class="flex justify-start "> 
+            <img src="https://supabase.com/_next/image?url=%2Fimages%2Flaunchweek%2F8%2FLW8-gradient.png&w=3840&q=75" alt="null"
+            class="absolute inset-0 w-full h-full">
+            <article class=" flex flex-col mt-32 ml-20 items-center">
+                <h1 class="text-6xl text-white font-bold m-2 lol">Learn Web-dev</h1>
+                <span class="text-6xl font-bold text-gray-300 m-2 lol">Faster and
+                    <em class="gradient group">
+                        Funner.
+                        <span class="absolute scale-0 group-hover:scale-100 bg-slate-900 rounded-lg border border-slate-600 text-white
+                        font-semibold p-2 m-2 text-lg left-1/3 top-1/2 transition duration-400 origin-bottom ">
+                            funner is a word
+                        </span>        
+                    </em>
+                </span> 
+                <p class="text-2xl font-semibold text-gray-400 flex flex-col items-center m-2">
+                    <span>W3schools without the <em class="gradient2">boredom.</em></span>
+                    <span>Fireship.io without the <em class="gradient3">29$/month</em></span>
+                </p>
+                <a href="/react" class="text-white bg-[#2a6786] p-2 text-2xl font-semibold rounded hover:bg-[#295063]
+                transition duration-300 m-4 ">
+                    start here
+                </a>
+            </article>
+        </section>  
+        <section>
+            <article>
+                <h3>⚡Interactive</h3>
+                <p>
+                    You shouldn't spend hours watching tutorials or thousands of dollars on bootcamps. You just need to, ACTUALLY CODE.
+                    We(nexTutorial) provide an interactive tutorial where you can actively learn React. Powered by Stackblitz. 
+                </p>
+            </article>
+            <article>
+                <h3>Fun</h3>
+                <p>
+                    Our free courses follow a linear stucture. Each lesson having a corresponding short video to explain the concepts
+                    visually. Check out the <a href="/games">games</a>.
+                </p>
+            </article>
+        </section> 
+        <section>
+            <h2>Some Cool Examples:</h2>
+            <!-- fill this out in the future -->
+        </section>
 
     
     
